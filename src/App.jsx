1edcecd20b98da3ff4b310151475b37f5376a353
Djennkins <djennkins228@gmail.com>
2024-03-29 17:52:25 +0200

    added useMemo and used better way to store stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Papa from "papaparse";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Pie } from "react-chartjs-2";
 
 function App() {
-	const [data, setData] = useState([]);
-	const [lost, setLost] = useState([]);
-	const [won, setWon] = useState([]);
-	const [open, setOpen] = useState([]);
+	const [stats, setStats] = useState({ lost: 0, won: 0, open: 0 });
 
 	const handleFileUpload = (e) => {
 		const file = e.target.files[0];
 		Papa.parse(file, {
 			header: true,
 			complete: (results) => {
-				setData(results.data);
-				setLost(results.data.filter((row) => row.status === "lost"));
-				setWon(results.data.filter((row) => row.status === "won"));
-				setOpen(results.data.filter((row) => row.status === "open"));
+				const counts = { lost: 0, won: 0, open: 0 };
+				results.data.forEach((row) => {
+					counts[row.status] = counts[row.status] + 1 || 1;
+				});
+				setStats(counts);
 			},
 		});
 	};
 
-	const doughnutdata = {
-		labels: [`LOST : ${lost.length}`, `WON: ${won.length}`, `OPEN: ${open.length}`],
-		datasets: [
-			{
-				label: "Deal Statistic",
-				data: [lost.length, won.length, open.length],
-				backgroundColor: ["rgb(199, 4, 4)", "rgb(0, 209, 7)", "rgb(255, 255, 0)"],
-				hoverOffset: 4,
-			},
-		],
-	};
+	const doughnutData = useMemo(() => {
+		return {
+			labels: [`LOST : ${stats.lost}`, `WON: ${stats.won}`, `OPEN: ${stats.open}`],
+			datasets: [
+				{
+					label: "Deal Statistic",
+					data: [stats.lost, stats.won, stats.open],
+					backgroundColor: ["rgb(199, 4, 4)", "rgb(0, 209, 7)", "rgb(255, 255, 0)"],
+					hoverOffset: 4,
+				},
+			],
+		};
+	}, [stats]);
 
 	const options = {
 		plugins: {
@@ -47,8 +47,8 @@ function App() {
 	return (
 		<div className=" flex flex-col items-center my-5 gap-5">
 			<input type="file" accept=".csv" onChange={handleFileUpload} />
-			<div className=" w-[700px] h-[700px] ">
-				{data.length > 0 && <Pie data={doughnutdata} options={options} />}
+			<div className="  w-[700px] h-[700px] ">
+				{(stats.lost > 0 || stats.won > 0 || stats.open > 0) && <Pie data={doughnutData} options={options} />}
 			</div>
 		</div>
 	);

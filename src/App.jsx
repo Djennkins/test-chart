3e741f300a4be3657c019ac2190dddb9b3feb2f4
Djennkins <djennkins228@gmail.com>
2024-03-29 16:39:27 +0200

    created pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Papa from "papaparse";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Pie } from "react-chartjs-2";
 
 function App() {
 	const [data, setData] = useState([]);
@@ -20,9 +22,34 @@ function App() {
 		});
 	};
 
+	const doughnutdata = {
+		labels: [`LOST : ${lost.length}`, `WON: ${won.length}`, `OPEN: ${open.length}`],
+		datasets: [
+			{
+				label: "Deal Statistic",
+				data: [lost.length, won.length, open.length],
+				backgroundColor: ["rgb(199, 4, 4)", "rgb(0, 209, 7)", "rgb(255, 255, 0)"],
+				hoverOffset: 4,
+			},
+		],
+	};
+
+	const options = {
+		plugins: {
+			legend: {
+				labels: {
+					color: "white",
+				},
+			},
+		},
+	};
+
 	return (
-		<div className="w-full flex flex-col items-center">
+		<div className=" flex flex-col items-center my-5 gap-5">
 			<input type="file" accept=".csv" onChange={handleFileUpload} />
+			<div className=" w-[700px] h-[700px] ">
+				{data.length > 0 && <Pie data={doughnutdata} options={options} />}
+			</div>
 		</div>
 	);
 }

d95b0f2ddf16c2fa4d2e8a9b6300ffd013d3b27c
Djennkins <djennkins228@gmail.com>
2024-03-29 14:13:36 +0200

    implemented input for csv file and filter data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import Papa from "papaparse";
+
 function App() {
-	return <></>;
+	const [data, setData] = useState([]);
+	const [lost, setLost] = useState([]);
+	const [won, setWon] = useState([]);
+	const [open, setOpen] = useState([]);
+
+	const handleFileUpload = (e) => {
+		const file = e.target.files[0];
+		Papa.parse(file, {
+			header: true,
+			complete: (results) => {
+				setData(results.data);
+				setLost(results.data.filter((row) => row.status === "lost"));
+				setWon(results.data.filter((row) => row.status === "won"));
+				setOpen(results.data.filter((row) => row.status === "open"));
+			},
+		});
+	};
+
+	return (
+		<div className="w-full flex flex-col items-center">
+			<input type="file" accept=".csv" onChange={handleFileUpload} />
+		</div>
+	);
 }
 
 export default App;
